064c01f407ab6c2031d3d4715e4a64cc9e11c3b8
Leo Dong <52641784+nxtloveev3@users.noreply.github.com>
2020-05-18 12:27:07 -0400

    Add files via upload

--- NBO.py
+++ NBO.py
@@ -1,36 +1,21 @@
-def readlines(file):
-    f = open(file,'rb')
-    lines = f.readlines()
-    lines = [line.decode("utf-8").strip() for line in lines]
-    return lines
-
-# This function returns the index of specific line based on key phrase. It's primarily used 
-# for finding different section of the file     
-def find(text,file):
-    result = []
-    count = 0
-    for elem in file:
-        if text in elem:
-            result.append(count)
-        count += 1
-    return result
+from lib import basicReadingFunctions
 
 # These two function extract the information needed and seperate them to different seperate files
 # based on the origin file(weather it is a triplet or singlet)
 def tripletFile(file):
-    nboSumStart = find("NATURAL BOND ORBITALS (Summary):",file)
-    startNAO = find("NATURAL POPULATIONS:  Natural atomic orbital occupancies",file)
-    endNAO = find("Summary of Natural Population Analysis:",file)
-    startNBOalpha = find("NATURAL BOND ORBITAL ANALYSIS, alpha spin orbitals:",file)
-    startNBObeta = find("NATURAL BOND ORBITAL ANALYSIS, beta spin orbitals:",file)
-    endNBO = find("NHO DIRECTIONALITY AND BOND BENDING",file)
-    startCMO = find("CMO: NBO Analysis of Canonical Molecular Orbitals",file)
-    endCMO = find("Molecular Orbital Atom-Atom Bonding Character",file)
-    startPert = find("SECOND ORDER PERTURBATION THEORY ANALYSIS OF FOCK MATRIX IN NBO BASIS",file)
-    endPert = find("NATURAL BOND ORBITALS (Summary):",file)
-    startNLMO = find("NATURAL LOCALIZED MOLECULAR ORBITAL (NLMO) ANALYSIS:",file)
-    endNLMO = find("NATURAL POPULATIONS:  Natural atomic orbital occupancies",file)
-    endNLMO2 = find("NBO analysis completed",file) 
+    nboSumStart = basicReadingFunctions.find("NATURAL BOND ORBITALS (Summary):",file)
+    startNAO = basicReadingFunctions.find("NATURAL POPULATIONS:  Natural atomic orbital occupancies",file)
+    endNAO = basicReadingFunctions.find("Summary of Natural Population Analysis:",file)
+    startNBOalpha = basicReadingFunctions.find("NATURAL BOND ORBITAL ANALYSIS, alpha spin orbitals:",file)
+    startNBObeta = basicReadingFunctions.find("NATURAL BOND ORBITAL ANALYSIS, beta spin orbitals:",file)
+    endNBO = basicReadingFunctions.find("NHO DIRECTIONALITY AND BOND BENDING",file)
+    startCMO = basicReadingFunctions.find("CMO: NBO Analysis of Canonical Molecular Orbitals",file)
+    endCMO = basicReadingFunctions.find("Molecular Orbital Atom-Atom Bonding Character",file)
+    startPert = basicReadingFunctions.find("SECOND ORDER PERTURBATION THEORY ANALYSIS OF FOCK MATRIX IN NBO BASIS",file)
+    endPert = basicReadingFunctions.find("NATURAL BOND ORBITALS (Summary):",file)
+    startNLMO = basicReadingFunctions.find("NATURAL LOCALIZED MOLECULAR ORBITAL (NLMO) ANALYSIS:",file)
+    endNLMO = basicReadingFunctions.find("NATURAL POPULATIONS:  Natural atomic orbital occupancies",file)
+    endNLMO2 = basicReadingFunctions.find("NBO analysis completed",file) 
     nboSumAlpha = file[nboSumStart[0]:startNLMO[0]]
     nboSumBeta = file[nboSumStart[1]:startNLMO[1]]
     nlmoAlpha = file[startNLMO[0]:endNLMO[0]]
@@ -47,17 +32,17 @@ def tripletFile(file):
     return (nboSumAlpha,nboSumBeta,nlmoAlpha,nlmoBeta,naoAll,naoAlpha,naoBeta,nboAlpha,nboBeta,cmoAlpha,cmoBeta,pertAlpha,pertBeta)    
     
 def singletFile(file):
-    nboSumStart = find("NATURAL BOND ORBITALS (Summary):",file)
-    startNAO = find("NATURAL POPULATIONS:  Natural atomic orbital occupancies",file)
-    endNAO = find("Summary of Natural Population Analysis:",file)
-    startNBOalpha = find("NATURAL BOND ORBITAL ANALYSIS",file)
-    endNBO = find("NHO DIRECTIONALITY AND BOND BENDING",file)
-    startCMO = find("CMO: NBO Analysis of Canonical Molecular Orbitals",file)
-    endCMO = find("Molecular Orbital Atom-Atom Bonding Character",file)
-    startPert = find("SECOND ORDER PERTURBATION THEORY ANALYSIS OF FOCK MATRIX IN NBO BASIS",file)
-    endPert = find("NATURAL BOND ORBITALS (Summary):",file)
-    startNLMO = find("NATURAL LOCALIZED MOLECULAR ORBITAL (NLMO) ANALYSIS:",file)
-    endNLMO2 = find("NBO analysis completed",file) 
+    nboSumStart = basicReadingFunctions.find("NATURAL BOND ORBITALS (Summary):",file)
+    startNAO = basicReadingFunctions.find("NATURAL POPULATIONS:  Natural atomic orbital occupancies",file)
+    endNAO = basicReadingFunctions.find("Summary of Natural Population Analysis:",file)
+    startNBOalpha = basicReadingFunctions.find("NATURAL BOND ORBITAL ANALYSIS",file)
+    endNBO = basicReadingFunctions.find("NHO DIRECTIONALITY AND BOND BENDING",file)
+    startCMO = basicReadingFunctions.find("CMO: NBO Analysis of Canonical Molecular Orbitals",file)
+    endCMO = basicReadingFunctions.find("Molecular Orbital Atom-Atom Bonding Character",file)
+    startPert = basicReadingFunctions.find("SECOND ORDER PERTURBATION THEORY ANALYSIS OF FOCK MATRIX IN NBO BASIS",file)
+    endPert = basicReadingFunctions.find("NATURAL BOND ORBITALS (Summary):",file)
+    startNLMO = basicReadingFunctions.find("NATURAL LOCALIZED MOLECULAR ORBITAL (NLMO) ANALYSIS:",file)
+    endNLMO2 = basicReadingFunctions.find("NBO analysis completed",file) 
     nboSum = file[nboSumStart[0]:startNLMO[0]]
     nlmo = file[startNLMO[0]:endNLMO[0]]
     nao = file[startNAO[0]:endNAO[0]]
@@ -68,7 +53,7 @@ def singletFile(file):
 
 class nbo(object):
     def __init__(self,file):
-        lines = readlines(file)
+        lines = basicReadingFunctions.readlines(file)
         if "alpha spin orbitals" or "beta spin orbitals" in lines:
             self.nboSumAlpha,self.nboSumBeta,self.nlmoAlpha,self.nlmoBeta,self.naoAll,self.naoAlpha,self.naoBeta,self.nboAlpha,self.nboBeta,self.cmoAlpha,self.cmoBeta,self.pertAlpha,self.pertBeta = tripletFile(lines)
         else:
