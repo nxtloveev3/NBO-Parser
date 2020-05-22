7895d6fec0a4e58d0fe298e4da9e7e3b5be127fb
Leo Dong <52641784+nxtloveev3@users.noreply.github.com>
2020-05-22 17:06:16 -0400

    Add files via upload

--- lib/parseNBO.py
+++ lib/parseNBO.py
@@ -1,4 +1,4 @@
-from basicReadingFunctions import readlines, find
+from .basicReadingFunctions import readlines, find, replacingDigit, fixEnding
 
 lines = readlines("CN01_NN13_S_nbo.log")
 nboSumStart = find("NATURAL BOND ORBITALS (Summary):",lines)
@@ -10,16 +10,14 @@ def parseNBO(nboSum):
     c1 = [] #This part trying to find first element of each line in the file
     for line in nboSum:
         if len(line) > 0: 
-            line = line.split()
-            if line[0].endswith("."): #Treat the number so it is easier to recognize as digit (Ex: 120. -> 120)
-                line[0] = line[0][:-1]
+            line = replacingDigit(0,line)
             c1.append(line[0])
         else:
             c1.append(line)
     posOrb = [] #Find the location of lines that have valuable infromation
-    for elem in c1:
-        if elem.isdigit():
-            posOrb.append(c1.index(elem))
+    for i in range(len(c1)):
+        if c1[i].isdigit():
+            posOrb.append(i)
     tmpOrb = []
     for index in posOrb:
         tmpOrb.append(nboSum[index])
@@ -28,6 +26,16 @@ def parseNBO(nboSum):
     posLV = find("LV",tmpOrb)
     posBD = find("BD",tmpOrb)
     posBDS = find("BD*(",tmpOrb)
-    
+    pos3C = find("3C",tmpOrb)
+    pos3Cn = find("3Cn(",tmpOrb)
+    pos3Cs = find("3C*(",tmpOrb)
+    tabCR = fixEnding(posCR,tmpOrb)
+    tabLP = fixEnding(posLP,tmpOrb)
+    tabLV = fixEnding(posLV,tmpOrb)
+    tabBD = fixEnding(posBD,tmpOrb)
+    tabBDS = fixEnding(posBDS,tmpOrb)
+    tab3C = fixEnding(pos3C,tmpOrb)
+    tab3Cn = fixEnding(pos3Cs,tmpOrb)
+    tab3Cs = fixEnding(pos3Cn,tmpOrb)
 
 parseNBO(nboSum)
