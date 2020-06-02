93ed635aecd50af12e8086f052ad51ee2d410f43
yaron <yaron@cmu.edu>
2020-06-02 18:01:47 -0400

    reparse.py uses regular expressions to parse an NAO table

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from lib.parseCMO import *
 singlet = "CN01_NN13_S_nbo.log"
 triplet = "CN01_NN01_T_nbo.log"
 
-#table = nbo(singlet)
-#cmo = table.cmo
-#nbo = table.nbo
+table = nbo(singlet)
+cmo = table.cmo
+nbo = table.nbo
 
 #table = nbo(triplet)
 #cmoAlpha = table.cmoA
@@ -15,8 +15,8 @@ triplet = "CN01_NN01_T_nbo.log"
 #nboAlpha = table.nboA
 #nboBeta = table.nboB
 
-#cmoTab = parseCMO(cmo)
+cmoTab = parseCMO(cmo)
 #nboTab = parseNBO(nbo)
-#print(cmoTab)
+print(cmoTab)
 
 
