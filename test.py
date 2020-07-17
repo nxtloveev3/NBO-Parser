21baee7b9119eacfc44120554f7f5d8439a85b1f
Leo Dong <52641784+nxtloveev3@users.noreply.github.com>
2020-07-16 20:39:54 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -5,31 +5,34 @@ from lib.parseCMO import *
 from lib.basicReadingFunctions import *
 from lib.parsePert import*
 from lib.parseNLMO import*
+from lib.parseNAO import*
 import pandas as pd
 import numpy as np
 
 singlet = "CN01_NN13_S_nbo.log"
 triplet = "CuII_NBO7_Br.log"
 
-#table = nbo(singlet)
+table = nbo(singlet)
 #cmo = table.cmo
 #nboSum = table.nboSum
-table = nbo(triplet)
+nao = table.nao
+#table = nbo(triplet)
 #cmoAlpha = table.cmoA
 #cmoBeta = table.cmoB
-nboSumAlpha = table.nboSumAlpha
-nboSumBeta = table.nboSumBeta
+#nboSumAlpha = table.nboSumAlpha
+#nboSumBeta = table.nboSumBeta
 #nboPertA = table.pertAlpha
 #nboNlmoA = table.nlmoAlpha
 
-tabCRFA,tabLPFA,tabLVFA,tabBDFA,tabBDSFA,tab3CFA,tab3CnFA,tab3CsFA = parseNboSum(nboSumAlpha)
-tabCRFB,tabLPFB,tabLVFB,tabBDFB,tabBDSFB,tab3CFB,tab3CnFB,tab3CsFB = parseNboSum(nboSumBeta)
-#tabCRF,tabBDF,tabBDSF,tab3CF,tab3CnF,tab3CsF = parseNBO(nboSum)  
+#tabCRFA,tabLPFA,tabLVFA,tabBDFA,tabBDSFA,tab3CFA,tab3CnFA,tab3CsFA = parseNboSum(nboSumAlpha)
+#tabCRFB,tabLPFB,tabLVFB,tabBDFB,tabBDSFB,tab3CFB,tab3CnFB,tab3CsFB = parseNboSum(nboSumBeta)
+#tableCRF,tableLPF,tableLVF,tableBDF,tableBDSF,table3CF,table3CnF,table3CsF = parseNboSum(nboSum)  
 #nboA = parseNBO(nboAlpha)
 #tableNBOA = pd.DataFrame(nboA)
 
 #pertA = parsePert(nboPertA)
 
 #nboNLMOA = parseNLMO(nboNlmoA)
-print(tabLPFA)
-print(tabLPFB)
+tabNAO = parseNAO(nao)
+tableNAO = pd.DataFrame(tabNAO)
+print(tableNAO)
